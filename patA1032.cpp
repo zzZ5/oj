f03a7d93a9863af7e4c8ae5d51f7ad532d67b1e6
baoju <baoju_liu@foxmail.com>
2019-03-03 14:58:36 +0800

    patA1032

---
+++ patA1032.cpp
@@ -0,0 +1,39 @@
+#include<cstdio>
+#include<cstring>
+const int max=100000;
+struct NODE
+{
+    char data;
+    int next;
+    bool flag;
+}node[max];
+
+int main(){
+    for(int i=0;i<max;i++){
+        node[i].flag=false;
+    }
+    int s1,s2,n;
+    scanf("%d%d%d",&s1,&s2,&n);
+    int adress,next;
+    char data;
+    for(int i=0;i<n;i++){
+        scanf("%d %c %d",&adress,&data,&next);
+        node[adress].data=data;
+        node[adress].next=next;
+    }
+    int p;
+    for(p=s1;p!=-1;p=node[p].next){
+        node[p].flag=true;
+    }
+    for(p=s2;p!=-1;p=node[p].next){
+        if(node[p].flag){
+            break;
+        }
+    }
+    if(p!=-1){
+        printf("%05d\n",p);
+    }else{
+        printf("-1\n");
+    }
+    return 0;
+}
