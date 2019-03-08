c3472be58dd85fb76cc1a628ea169c1e8820a093
baoju <baoju_liu@foxmail.com>
2019-03-08 10:28:57 +0800

    criticalPath

--- others/criticalPath.cpp
+++ others/criticalPath.cpp
@@ -1,12 +1,19 @@
 #include <cstdio>
 #include <vector>
+#include <stack>
 #include <queue>
 #include <algorithm>
 using namespace std;
 const int MAXV = 101;
+struct Edge {
+    int node, weight;
+};
 
-vector<int> G[MAXV];
-int nodeNum, edgeNum, inDegree[MAXV];
+vector<Edge> G[MAXV];
+int nodeNum, edgeNum, inDegree[MAXV], ve[MAXV], vl[MAXV];
+stack<int> topOrder;
+
+
 
 bool topologicalSort() {
     int num = 0;
@@ -20,29 +27,66 @@ bool topologicalSort() {
         int u = q.front();
         printf("%d", u);
         q.pop();
+        topOrder.push(u);
         for(int i = 0; i < G[u].size(); i++) {
-            int v = G[u][i];
+            int v = G[u][i].node;
             inDegree[v]--;
             if(inDegree[v] == 0){
                 q.push(v);
             }
+            if(ve[u] + G[u][i].weight > ve[v]) {
+               ve[v] =  ve[u] + G[u][i].weight;
+            }
         }
-        G[u].clear();
-        num++;
     }
-    return num == nodeNum;
+    return topOrder.size() == nodeNum;
+}
+
+int criticalPath() {
+    fill(ve, ve + MAXV, 0);
+    if(!topologicalSort()){
+        return -1;
+    }
+    fill(vl, vl + MAXV, ve[nodeNum-1]);
+    while(!topOrder.empty()) {
+        int u = topOrder.top();
+        topOrder.pop();
+        for(int i = 0; i < G[u].size(); i++) {
+            int v = G[u][i].node;
+            if(vl[v] - G[u][i].weight < vl[u]) {
+                vl[u] = vl[v] - G[u][i].weight;
+            }
+        }
+    }
+
+    for(int u = 0; u < nodeNum; u++) {
+        for(int i = 0; i < G[u].size(); i++) {
+            int v = G[u][i].node, w = G[u][i].weight;
+            int e = ve[u], l = vl[v] - w;
+            if(e == l) {
+                printf("%d->%d\n", u , v);
+            }
+
+        }
+    }
+    return ve[nodeNum-1];
 }
 
 
+
 int main() {
-    int u, v;
+    int u, v, w;
     fill(inDegree, inDegree + MAXV, 0);
     scanf("%d%d", &nodeNum, &edgeNum);
     for(int i = 0; i < edgeNum; i++) {
-        scanf("%d%d", &u, &v);
-        G[u].push_back(v);
+        scanf("%d%d%d", &u, &v, &w);
+        Edge edge = {
+            .node = v,
+            .weight = w
+        };
+        G[u].push_back(edge);
         inDegree[v]++;
     }
-    topologicalSort();
+    criticalPath();
     return 0;
 }
