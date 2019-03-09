66b598d713272ee6127fabe22c3b846094556825
baoju <baoju_liu@foxmail.com>
2019-03-09 10:06:13 +0800

    LCS

--- others/LCS.cpp
+++ others/LCS.cpp
@@ -1,35 +1,52 @@
 //最长公共子序列 (Longest common subsequence)
+// #include <cstdio>
+// #include <cstring>
+// #include <algorithm>
+// using namespace std;
+// const int MAXN = 101;
+// char A[MAXN], B[MAXN];
+// int dp[MAXN][MAXN];
+
+// int main() {
+//     int n;
+//     gets(A + 1); //从下标1开始读取
+//     gets(B + 1);
+//     int lenA = strlen(A + 1);
+//     int lenB = strlen(B + 1);
+
+//     for(int i = 0; i < lenA; i++) {
+//         dp[i][0] = 0;
+//     }
+//     for(int i = 0; i < lenB; i++) {
+//         dp[0][i] = 0;
+//     }
+
+//     for(int i = 1; i <= lenA; i++) {
+//         for(int j = 1; j <= lenB; j++) {
+//             if(A[i] == B[j]) {
+//                 dp[i][j] = dp[i-1][j-1] +1;
+//             } else {
+//                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//             }
+//         }
+//     }
+//     printf("%d\n", dp[lenA][lenB]);
+//     return 0;
+// }
+
+#include <iostream>
 #include <cstdio>
 #include <cstring>
+#include <map>
+#include <vector>
 #include <algorithm>
 using namespace std;
-const int MAXN = 101;
-char A[MAXN], B[MAXN];
-int dp[MAXN][MAXN];
+typedef long long LL;
+
+
 
 int main() {
-    int n;
-    gets(A + 1); //从下标1开始读取
-    gets(B + 1);
-    int lenA = strlen(A + 1);
-    int lenB = strlen(B + 1);
 
-    for(int i = 0; i < lenA; i++) {
-        dp[i][0] = 0;
-    }
-    for(int i = 0; i < lenB; i++) {
-        dp[0][i] = 0;
-    }
 
-    for(int i = 1; i <= lenA; i++) {
-        for(int j = 1; j <= lenB; j++) {
-            if(A[i] == B[j]) {
-                dp[i][j] = dp[i-1][j-1] +1;
-            } else {
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-    }
-    printf("%d\n", dp[lenA][lenB]);
     return 0;
 }
