72066bbe3ad4bae7a20f19d1c16b23b3e5903ca1
baoju <baoju_liu@foxmail.com>
2019-03-15 09:27:12 +0800

    Sort with Swap(0,*)

--- patA/patA1067.cpp
+++ patA/patA1067.cpp
@@ -1,5 +1,36 @@
 #include <cstdio>
+#include <algorithm>
+using namespace std;
+const int MAXN = 100001;
 
 int main() {
+    int n, pos[MAXN], num;
+    scanf("%d", &n);
+    int disloc = n - 1;
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        pos[num] = i;
+        if(num == i && num != 0) disloc--;
+    }
+    int ans = 0;
+    int temp = 1;
+    while(disloc > 0) {
+        if(pos[0] == 0) {
+            while(temp < n) {
+                if(pos[temp] != temp) {
+                    swap(pos[0], pos[temp]);
+                    ans++;
+                    break;
+                }
+                temp++;
+            }
+        }
+        while(pos[0] != 0) {
+            swap(pos[0], pos[pos[0]]);
+            ans++;
+            disloc--;
+        }
+    }
+    printf("%d\n", ans);
     return 0;
 }
