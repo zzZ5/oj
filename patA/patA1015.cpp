23d6f2d8731db8ddd74281f8fb92767ee11e7fe3
baoju <baoju_liu@foxmail.com>
2019-03-16 16:31:44 +0800

    Reversible Primes

---
+++ patA/patA1015.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <math.h>
+
+bool isPrime(int n) {
+    if(n <= 1) return false;
+    int sqr = (int)sqrt(1.0 * n);
+    for(int i = 2; i <= sqr; i++) {
+        if(n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int d[101];
+
+int main() {
+    int n, radix;
+    while(scanf("%d", &n) != EOF) {
+        if(n < 0) break;
+        scanf("%d", &radix);
+        if(isPrime(n) == false) {
+            printf("No\n");
+        } else {
+            int len = 0;
+            do {
+                d[len++] = n % radix;
+                n /= radix;
+            } while(n != 0);
+            for(int i = 0; i < len; i++) {
+                n = n * radix + d[i];
+            }
+            if(isPrime(n)) printf("Yes\n");
+            else printf("No\n");
+        }
+    }
+    return 0;
+}
