65718a0c6e1f425bc9cef2b986ad08732b5d27d4
baoju <baoju_liu@foxmail.com>
2019-03-16 10:31:50 +0800

    数字黑洞

--- patB/patB1019.cpp
+++ patB/patB1019.cpp
@@ -1,37 +1,37 @@
-#include<cstdio>
-#include<algorithm>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
-const int DIGIT=4;
-    bool cmp(int a,int b){
-        return a>b;
+const int DIGIT = 4;
+    
+bool cmp(int a,int b) {
+    return a > b;
+}
+void toArray(int n, int num[]) {
+    for(int i = 0; i < DIGIT; i++){
+        num[i] = n % 10;
+        n = n / 10;
     }
-    void toArray(int n,int num[]){
-        for(int i=0;i<DIGIT;i++){
-            num[i]=n%10;
-            n=n/10;
-        }
+}
+int toNumber(int num[]) {
+    int n = 0;
+    for(int i = 0; i < DIGIT; i++){
+        n = n * 10 + num[i];
     }
-    int toNumber(int num[]){
-        int n=0;
-        for(int i=0;i<DIGIT;i++){
-            n=n*10+num[i];
-        }
-        return n;
-    }
+    return n;
+}
 
 int main(){
-    int n,min,max;
+    int n, min, max;
     int num[DIGIT];
-    scanf("%d",&n);
-    do{
-        toArray(n,num);
-        sort(num,num+DIGIT);
-        min=toNumber(num);
-        sort(num,num+DIGIT,cmp);
-        max=toNumber(num);
-        n=max-min;
-        printf("%04d - %04d = %04d\n",max,min,n);
-    }while(n!=0 && n!=6174);
-    
+    scanf("%d", &n);
+    do {
+        toArray(n, num);
+        sort(num, num + DIGIT);
+        min = toNumber(num);
+        sort(num, num + DIGIT, cmp);
+        max = toNumber(num);
+        n = max - min;
+        printf("%04d - %04d = %04d\n", max, min, n);
+    }while(n != 0 && n != 6174);
     return 0;
 }
