387ef93c876c03808546d48aa8c99af529dcda57
baoju <baoju_liu@foxmail.com>
2019-03-16 16:07:02 +0800

    数素数

--- patB/patB1013.cpp
+++ patB/patB1013.cpp
@@ -1,31 +1,29 @@
 #include<cstdio>
-const int max=1000001;
-int prime[max],num=0;
-bool p[max]={false};
+const int max = 1000001;
+int prime[max], num = 0;
+bool p[max] = {false};
 
-void findPrime(int n){
-    for(int i=2;i<max;i++){
+void findPrime(int n) {
+    for(int i = 2; i < max; i++){
         if(!p[i]){
-            prime[num++]=i;
-            if(num>n){
-                break;
-            }
-            for(int j=i+i;j<max;j+=i){
-                p[j]=true;
+            prime[num++] = i;
+            if(num>n) break;
+            for(int j = i + i; j < max; j += i){
+                p[j] = true;
             }
         }
     }
 }
 
 int main(){
-    int m,n;
-    scanf("%d%d",&m,&n);
+    int m, n;
+    scanf("%d%d", &m, &n);
     findPrime(n);
-    int count=0;
-    for(int i=m-1;i<n;i++){
+    int count = 0;
+    for(int i = m - 1; i < n; i++){
         printf("%d",prime[i]);
         count++;
-        if(count%10==0 || i==n-1){
+        if(count % 10 == 0 || i == n-1){
             printf("\n");
         }else{
             printf(" ");
