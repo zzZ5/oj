8da9ea37c5201f50cd78e56546648a119caa4d38
baoju <baoju_liu@foxmail.com>
2019-03-16 17:52:19 +0800

    Prime Factors

--- patA/patA1059.cpp
+++ patA/patA1059.cpp
@@ -1,69 +1,61 @@
-#include<cstdio>
-#include<cmath>
+#include <cstdio>
+#include <math.h>
 
-const int MAX=100010;
-int prime[MAX],pNum=0;
+const int MAX = 100010;
+int prime[MAX], pNum=0;
 
-struct factor{
-   int x,cnt;
+struct factor {
+   int x, cnt;
 }fac[10];
 
-bool isPrime(int n){
-    bool flag=(n==1?false:true);
-    int sqr=(int)sqrt(1.0*n);
-    for(int i=2;i<=sqr;i++){
-        if(n%i==0){
-            flag=false;
-            break;
-        }
+bool isPrime(int n) {
+    if(n <= 1) return false;
+    int sqr = (int)sqrt(1.0 * n);
+    for(int i = 2; i <= sqr; i++){
+        if(n % i==0) return false;
     }
-    return flag;
+    return true;
 }
 
-void findPrime(){
-    for(int i=1;i<MAX;i++){
+void findPrime() {
+    for(int i = 1; i < MAX; i++){
         if(isPrime(i)){
-            prime[pNum++]=i;
+            prime[pNum++] = i;
         }
     }
 }
 
-int main(){
+int main() {
     findPrime();
-    int n,num;
-    scanf("%d",&n);
-    if(n==1){
-     printf("1=1");
-    }    
-    else {
-        printf("%d=",n);
-        int sqr=(int)sqrt(1.0*n);
-        for(int i=0;i<pNum && prime[i]<=sqr;i++){
-            if(n%prime[i]==0){
-                fac[num].x=prime[i];
-                fac[num].cnt=0;
-                while(n%prime[i]==0){
-                    n/=prime[i];
+    int n, num;
+    scanf("%d", &n);
+    if(n == 1) {
+        printf("1=1");
+    } else {
+        printf("%d=", n);
+        int sqr = (int)sqrt(1.0 * n);
+        for(int i = 0; i < pNum && prime[i] <= sqr; i++){
+            if(n % prime[i] == 0){
+                fac[num].x = prime[i];
+                fac[num].cnt = 0;
+                while(n % prime[i] == 0){
+                    n /= prime[i];
                     fac[num].cnt++;
                 }
                 num++;
             }
-            if (n==1) {
-                break;
-            }
+            if (n==1) break;
         }
     }
     if(n!=1){
-        fac[num].x=n;
-        fac[num++].cnt=1;
+        fac[num].x = n;
+        fac[num++].cnt = 1;
     }
-    for(int i=0;i<num;i++){
-        if(i>0){
-            printf("*");
-        }
-        printf("%d",fac[i].x);
+    for(int i = 0; i < num; i++){
+        if(i > 0) printf("*");
+        printf("%d", fac[i].x);
         if(fac[i].cnt>1){
-            printf("^%d",fac[i].cnt);
+            printf("^%d", fac[i].cnt);
         }
     }
     return 0;
