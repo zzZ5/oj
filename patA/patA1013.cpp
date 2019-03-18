898381e6bf6f62ccee898b68fe6bdb49705896e5
baoju <baoju_liu@foxmail.com>
2019-03-18 18:50:40 +0800

    Battle Over Cities

--- patA/patA1013.cpp
+++ patA/patA1013.cpp
@@ -21,8 +21,22 @@ int n, m, k;
 int main() {
     scanf("%d%d%d", &n, &m, &k);
     for(int i = 0; i <m; i++) {
-        
+        int a, b;
+        scanf("%d%d", &a, &b);
+        G[a].push_back(b);
+        G[b].push_back(a);
     }
-
+    for(int i = 0; i < k; i++) {
+        scanf("%d", &curPoint);
+        memset(visit, false, sizeof(visit));
+        int block= 0;
+        for(int j = 1; j <= n; j++) {
+            if(i != curPoint && !visit[i]) {
+                dfs(i);
+                block++;
+            }
+        }
+        printf("%d\n", block - 1);
+    }
     return 0;
 }
