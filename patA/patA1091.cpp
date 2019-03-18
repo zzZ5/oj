c11c4e1515552f7e3c7f753f18c3b18fd2520fb8
baoju <baoju_liu@foxmail.com>
2019-03-18 09:20:03 +0800

    Acute Stroke

---
+++ patA/patA1091.cpp
@@ -0,0 +1,68 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int x, y, z;
+} node;
+
+int n, m, slice, T;
+int pixel[1290][130][61];
+bool visit[1290][130][61] = {false};
+int X[6] = {0, 0, 0, 0, 1, -1};
+int Y[6] = {0, 0, 1, -1, 0, 0};
+int Z[6] = {1, -1, 0, 0, 0, 0};
+
+bool judge(int x, int y, int z) {
+    if(x >= n || x < 0 || y >= m || y < 0 || z >= slice || z < 0) return false;
+    if(pixel[x][y][z] == 0 || visit[x][y][z]) return false;
+    return true;
+}
+
+int BFS(int x, int y, int z) {
+    int tot = 0;
+    queue<Node> q;
+    node.x = x, node.y = y, node.z = z;
+    q.push(node);
+    visit[x][y][z] = true;
+    while(!q.empty()) {
+        Node top = q.front();
+        q.pop();
+        tot++;
+        for(int i = 0; i < 6; i++) {
+            int newX = top.x + X[i];
+            int newY = top.y + Y[i];
+            int newZ = top.z + Z[i];
+            if(judge(newX, newY, newZ)) {
+                node.x = newX, node.y = newY, node.z = newZ;
+                q.push(node);
+                visit[newX][newY][newZ] = true;
+            }
+        }
+    }
+    if(tot >= T) return tot;
+    else return 0;
+}
+
+int main() {
+    scanf("%d%d%d%d", &n, &m, &slice, &T);
+    for(int z = 0; z < slice; z++) {
+        for(int x = 0; x < n; x++) {
+            for(int y = 0; y < m; y++) {
+                scanf("%d", &pixel[x][y][z]);
+            }
+        }
+    }
+    int ans = 0;
+    for(int z = 0; z < slice; z++) {
+        for(int x = 0; x < n; x++) {
+            for(int y = 0; y < m; y++) {
+                if(pixel[x][y][z] == 1 && !visit[x][y][z]) {
+                    ans += BFS(x, y, z);
+                }
+            }
+        }
+    }
+    printf("%d", ans);
+    return 0;
+}
